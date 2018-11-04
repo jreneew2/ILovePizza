f5ef2826fcf8343a46471e4e9c68c6132736be9d
Drew Williams <williams.r.drew@gmail.com>
2018-11-03 21:40:37 -0400

    fixed movement

--- Player.py
+++ Player.py
@@ -6,41 +6,62 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         #pygame.display.init()
         self.image, self.rect = load_png('kang.png')
-        #self.rect = self.image.get_rect()
-        self.rect = self.rect.inflate(-64, -64)
 
         self.isMoving = False
         self.key = pygame.K_m #can't create an empty variable or something smh
-        
-        self.x = 0
-        self.y = 415
-        self.rect.x = 0
-        self.rect.y = 415
+
+        self.velX = 0
+        self.velY = 0
+        self.accX = 0
+        self.accY = 0
+        self.posX = 100
+        self.posY = 100
+        self.rect.x = 100
+        self.rect.y = 100
+        self.speed = 500
 
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
 
-    def reinit(self):
-        self.x = 0
-        self.y = 415
-        self.rect.x = 0
-        self.rect.y = 415
+    def update_bounds(self):
+        self.rect.x = self.posX - self.image.get_width() / 2
+        self.rect.y = self.posY - self.image.get_height() / 2
 
-    def move(self, passKey):
-        self.isMoving = True
-        self.key = passKey
+    def input(self, up, down, left, right):
+        if(up):
+            self.velY = -self.speed
+        elif(down):
+            self.velY = self.speed
+        else:
+            self.velY = 0
+        if(left):
+            self.velX = -self.speed
+        elif(right):
+            self.velX = self.speed
+        else:
+            self.velX = 0
 
-    def draw(self, screen):
-        if self.isMoving:
-            if self.key == pygame.K_UP and self.rect.y > 10:
-                self.rect.y -= 10
-            elif self.key == pygame.K_DOWN and self.rect.y < 500:
-                self.rect.y += 10
-            elif self.key == pygame.K_RIGHT and self.rect.x < 1000:
-                self.rect.x += 10
-            elif self.key == pygame.K_LEFT and self.rect.x > 10:
-                self.rect.x -= 10
-                
-        screen.blit(self.image, (self.rect.x, self.rect.y))
+    def calc_pos(self, deltatime):
+        if(self.rect.right >= self.area.width or self.rect.left <= 0):
+            self.velX = self.velX * -1
+        if(self.rect.bottom >= self.area.height or self.rect.top <= 0):
+            self.velY = self.velY * -1
+
+        self.velX = self.velX + self.accX * deltatime
+        self.velY = self.velY + self.accY * deltatime
+
+        self.posX = self.posX + self.velX * deltatime
+        self.posY = self.posY + self.velY * deltatime
+
+        self.update_bounds()
+
+        print("velX: " + str(self.velX))
+        print("velY: " + str(self.velY))
+        print("posX: " + str(self.posX))
+        print("posY: " + str(self.posY))
+
+    def draw(self, screen): 
+        screen.blit(self.image, (self.posX - self.image.get_width() / 2, self.posY - self.image.get_height() / 2))
+
         
 
--- driver.py
+++ driver.py
@@ -21,12 +21,16 @@ class Game:
         self.p = Player()
         playerSprite = pygame.sprite.RenderPlain(self.p)
         self.p.draw(screen)
-        #myfont = pygame.font.SysFont('Times New Roman MS', 30)
-        #text = myfont.render("Wegman's Official Game OwO", 0, (255, 50, 20))
-
+        myfont = pygame.font.SysFont('Times New Roman MS', 30)
+        text = myfont.render("Wegman's Official Game OwO", 0, (255, 50, 20))
         clock = pygame.time.Clock()
         while not self.done:
-            clock.tick(60)
+            timedelta = clock.tick(60)
+            timedelta = timedelta / 1000
+            keystate = pygame.key.get_pressed()
+            self.p.input(keystate[pygame.K_w], keystate[pygame.K_s], keystate[pygame.K_a], keystate[pygame.K_d])
+            self.p.calc_pos(timedelta)
+            self.p.draw(screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.done = True
@@ -34,18 +38,16 @@ class Game:
                     passKey = event.key
                     self.getInput(passKey)
                     
-            #screen.blit(text, ((self.width/2)-60, self.height/2))
-            if event.type == pygame.KEYDOWN:
-                self.p.draw(screen)
-                pygame.display.update()
-                pygame.display.flip()
-                screen.blit(background, (0,0))
-            
+            screen.blit(text, ((self.width/2)-60, self.height/2))
+            pygame.display.update()
+            screen.blit(background, (0,0))
+
     def getInput(self, passKey):
         if passKey == pygame.K_q:
             self.done = True
         if passKey == pygame.K_UP or passKey == pygame.K_DOWN or passKey == pygame.K_LEFT or passKey == pygame.K_RIGHT:
-            self.p.move(passKey)
+            print("key")
+            #self.p.move(passKey)
             
 
             

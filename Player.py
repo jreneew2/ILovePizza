8d14f9d0929712d9a077be4e479a2ad469498227
Drew Williams <williams.r.drew@gmail.com>
2018-11-03 23:44:28 -0400

    working on collisions

--- Player.py
+++ Player.py
@@ -10,6 +10,9 @@ class Player(pygame.sprite.Sprite):
         self.isMoving = False
         self.key = pygame.K_m #can't create an empty variable or something smh
 
+        screen = pygame.display.get_surface()
+
+
         self.velX = 0
         self.velY = 0
         self.accX = 0
@@ -20,7 +23,6 @@ class Player(pygame.sprite.Sprite):
         self.rect.y = 100
         self.speed = 500
 
-        screen = pygame.display.get_surface()
         self.area = screen.get_rect()
 
     def update_bounds(self):
@@ -29,39 +31,59 @@ class Player(pygame.sprite.Sprite):
 
     def input(self, up, down, left, right):
         lastangle = 0
+        previousRect = self.rect
         if(up):
-            self.velY = self.speed
-            self.image, self.rect = load_png('playert.png')
-        elif(down):
             self.velY = -self.speed
+            self.image, self.rect = load_png('playert.png')
+            self.rect.x = previousRect.x
+            self.rect.y = previousRect.y
+        elif(down):
+            self.velY = self.speed
             self.image, self.rect = load_png('playerd.png')
+            self.rect.x = previousRect.x
+            self.rect.y = previousRect.y
         else:
             self.velY = 0
         if(left):
-            self.velX = self.speed
-            self.image, self.rect = load_png('playerl.png')
-        elif(right):
             self.velX = -self.speed
+            self.image, self.rect = load_png('playerl.png')
+            self.rect.x = previousRect.x
+            self.rect.y = previousRect.y
+            print(self.rect.x)
+            print(self.rect.y)
+        elif(right):
+            self.velX = self.speed
             self.image, self.rect = load_png('playerr.png')
+            self.rect.x = previousRect.x
+            self.rect.y = previousRect.y
         else:
             self.velX = 0
 
         if(up and right):
             self.image, self.rect = load_png('playertr.png')
+            self.rect.x = previousRect.x
+            self.rect.y = previousRect.y
         elif(up and left):
             self.image, self.rect = load_png('playertl.png')
+            self.rect.x = previousRect.x
+            self.rect.y = previousRect.y
         elif(down and left):
             self.image, self.rect = load_png('playerdl.png')
+            self.rect.x = previousRect.x
+            self.rect.y = previousRect.y
         elif(down and right):
             self.image, self.rect = load_png('playerdr.png')
-            
+            self.rect.x = previousRect.x
+            self.rect.y = previousRect.y          
 
     def calc_pos(self, deltatime):
-        if(self.rect.right >= self.area.width or self.rect.left <= 0):
-            print("out on sides")
+        if(self.rect.right > self.area.width or self.rect.left < 0):
+            print('left: ' + str(self.rect.left))
+            print('right: ' + str(self.rect.right))
             self.velX = 0
-        if(self.rect.bottom >= self.area.height or self.rect.top <= 0):
-            print("out on top or bottom")
+        if(self.rect.bottom > self.area.height or self.rect.top < 0):
+            print('top: ' + str(self.rect.top))
+            print('bottom: ' + str(self.rect.bottom))
             self.velY = 0
 
         self.velX = self.velX + self.accX * deltatime
@@ -74,7 +96,7 @@ class Player(pygame.sprite.Sprite):
 
     def draw(self, screen): 
         screen.blit(self.image, (self.posX - self.image.get_width() / 2, self.posY - self.image.get_height() / 2))
-        pygame.draw.rect(pygame.display.get_surface(), (0, 255, 0), self.rect)
+        pygame.draw.rect(pygame.display.get_surface(), (0, 255, 0), self.rect, 2)
 
         
 
